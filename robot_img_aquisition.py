b24255203302c0904b99a9500b11259ff9a0a718
EdvardBruun <ebruun@princeton.edu>
2022-03-03 11:50:51 -0500

    added 2nd pass shed stitch configs

--- robot_img_aquisition.py
+++ robot_img_aquisition.py
@@ -185,7 +185,7 @@ def robot_config_saving(rob_nums, i, n_config):
 if __name__ == "__main__":
 
     rob_nums = [1, 2]
-    robot_config_saving(rob_nums, i=3, n_config=7)
+    robot_config_saving(rob_nums, i=3, n_config=15)
     # calibration(rob_nums, pose_range=False)
     # stitching(rob_nums, pose_range=False)
     # stitching_shed(rob_nums, pose_range=range(5,7))
